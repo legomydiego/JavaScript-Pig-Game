b239222b1974506d3bf899165f86c62e8eecc4f3
Diego Vargas <diegofvargas@gmail.com>
2018-01-22 23:09:57 -0500

    Final score

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ document.querySelector('.btn-hold').addEventListener('click',function() {
         
 
         //check for victory condition
-        if(scores[activePlayer]>=20) {
+        if(scores[activePlayer]>=100) {
             document.querySelector('#name-'+activePlayer).textContent = 'WINNER';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
