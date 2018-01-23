f215bbf0f941fa27b7f96fe8b3d640440e20ddff
Diego Vargas <diegofvargas@gmail.com>
2018-01-22 20:33:07 -0500

    Game setup

--- app.js
+++ app.js
@@ -8,11 +8,44 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores,roundScore,activePlayer,dice;
+var scores,roundScore,activePlayer;
 scores = [0,0];
-roundscore = 0;
+roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random()*6)+1;
 
-document.querySelector('#current-'+activePlayer).textContent = dice;
 document.querySelector('.dice').style.display= 'none';
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click',function() {
+    var dice = Math.floor(Math.random()*6)+1;
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+dice+'.png';
+    
+    if(dice!==1){
+        //add score
+        roundScore += dice;
+        document.getElementById('current-'+ activePlayer).textContent = roundScore;
+    }else{
+        //next player
+        activePlayer ===0 ? activePlayer = 1: activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click',function() {
+    //add current score to global
+
+    //update the UI
+
+    //check for victory condition
+})
